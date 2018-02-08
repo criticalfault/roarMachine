978392f426c88dd10ac3149f1b704d7ae08f3be3
Dean Petty <dean.petty@gmail.com>
2018-02-07 21:59:31 -0600

    Added bottle functionality

--- roar.py
+++ roar.py
@@ -1,13 +1,22 @@
 import time
 import pygame
+from bottle import Bottle, route, run, template
 
 
-
+app = Bottle()
 pygame.init()
 
+
+
+@app.get('/')
+def index():
+	return template('index')
+
+@app.post('/roar')
 def roar():
     pygame.mixer.music.load('roar.mp3')
     pygame.mixer.music.play()
     time.sleep(10)
-    
-roar()
+
+
+run(app, host='localhost', port=8080)	
