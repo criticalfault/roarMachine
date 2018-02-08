2aa71a520a8d2329941c8e7380a7b1b23f585987
Dean Petty <dean.petty@gmail.com>
2018-02-07 22:02:12 -0600

    Added Stego roar

--- roar.py
+++ roar.py
@@ -14,9 +14,14 @@ def index():
 
 @app.post('/roar')
 def roar():
-    pygame.mixer.music.load('roar.mp3')
+    pygame.mixer.music.load('roar.wav')
     pygame.mixer.music.play()
     time.sleep(10)
 
+@app.post('/stego')
+def roar():
+    pygame.mixer.music.load('stegosaurus.wav')
+    pygame.mixer.music.play()
+    time.sleep(10)
 
 run(app, host='localhost', port=8080)	
